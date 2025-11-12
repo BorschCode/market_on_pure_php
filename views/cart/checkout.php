21d88b4c97c9b8b9ff9b01bde93c198f5877eeed
serega <serega@local.tuf>
2025-11-12 14:27:08 +0200

    Refactor(admin): Localize and document category and order CRUD views
    Translates admin UI text from Russian to English in the category update and order delete views, ensuring full English localization across these templates. Also adds PHPDoc for variables and styles the delete button.

--- views/cart/checkout.php
+++ views/cart/checkout.php
@@ -1,77 +1,88 @@
-<?php include ROOT . '/views/layouts/header.php'; ?>
+<?php
+/**
+ * @var array $categories      Array of categories for the left sidebar navigation.
+ * @var bool $result           Flag indicating if the order was successfully placed (true) or if the checkout form is shown (false).
+ * @var int $totalQuantity     Total number of items selected in the cart.
+ * @var float $totalPrice      Total price of all items in the cart.
+ * @var array|null $errors     Array of validation errors, if the form submission failed.
+ * @var string $userName       Value for the User Name input field.
+ * @var string $userPhone      Value for the Phone Number input field.
+ * @var string $userComment    Value for the Order Comment input field.
+ */
+include ROOT . '/views/layouts/header.php'; ?>
 
     <section>
-    <div class="container">
-    <div class="row">
-    <div class="col-sm-3">
-        <div class="left-sidebar">
-            <h2>Catalog</h2>
-            <div class="panel-group category-products">
-                <?php foreach ($categories as $categoryItem): ?>
-                    <div class="panel panel-default">
-                        <div class="panel-heading">
-                            <h4 class="panel-title">
-                                <a href="/alias/c<?php echo $categoryItem['id']; ?>">
-                                    <?php echo $categoryItem['name']; ?>
-                                </a>
-                            </h4>
+        <div class="container">
+            <div class="row">
+                <div class="col-sm-3">
+                    <div class="left-sidebar">
+                        <h2>Catalog</h2>
+                        <div class="panel-group category-products">
+                            <?php foreach ($categories as $categoryItem): ?>
+                                <div class="panel panel-default">
+                                    <div class="panel-heading">
+                                        <h4 class="panel-title">
+                                            <a href="/alias/c<?php echo $categoryItem['id']; ?>">
+                                                <?php echo $categoryItem['name']; ?>
+                                            </a>
+                                        </h4>
+                                    </div>
+                                </div>
+                            <?php endforeach; ?>
                         </div>
                     </div>
-                <?php endforeach; ?>
-            </div>
-        </div>
-    </div>
-
-            <div class="col-sm-9 padding-right">
-                <div class="features_items">
-                    <h2 class="title text-center">Cart</h2>
-
-
-                    <?php if ($result): ?>
-
-                        <p>Заказ оформлен. Мы Вам перезвоним.</p>
-
-                    <?php else: ?>
-
-                        <p>Выбрано товаров: <?php echo $totalQuantity; ?>, на сумму: <?php echo $totalPrice; ?>, грн.</p><br/>
-
-                        <div class="col-sm-4">
-                            <?php if (isset($errors) && is_array($errors)): ?>
-                                <ul>
-                                    <?php foreach ($errors as $error): ?>
-                                        <li> - <?php echo $error; ?></li>
-                                    <?php endforeach; ?>
-                                </ul>
-                            <?php endif; ?>
-
-                            <p>Для оформления заказа заполните форму. Наш менеджер свяжется с Вами.</p>
-
-                            <div class="login-form">
-                                <form action="#" method="post">
-
-                                    <p>Ваша имя</p>
-                                    <input type="text" name="userName" placeholder="" value="<?php echo $userName; ?>"/>
-
-                                    <p>Номер телефона</p>
-                                    <input type="text" name="userPhone" placeholder="" value="<?php echo $userPhone; ?>"/>
-
-                                    <p>Комментарий к заказу</p>
-                                    <input type="text" name="userComment" placeholder="Сообщение" value="<?php echo $userComment; ?>"/>
-
-                                    <br/>
-                                    <br/>
-                                    <input type="submit" name="submit" class="btn btn-default" value="Оформить" />
-                                </form>
-                            </div>
-                        </div>
-
-                    <?php endif; ?>
-
                 </div>
 
+                <div class="col-sm-9 padding-right">
+                    <div class="features_items">
+                        <h2 class="title text-center">Cart</h2>
+
+
+                        <?php if ($result): ?>
+
+                            <p>Your order has been placed. We will call you back soon.</p>
+
+                        <?php else: ?>
+
+                            <p>Selected items: <?php echo $totalQuantity; ?>, Total amount: <?php echo number_format($totalPrice, 2); ?> UAH.</p><br/>
+
+                            <div class="col-sm-4">
+                                <?php if (isset($errors) && is_array($errors)): ?>
+                                    <ul class="text-danger">
+                                        <?php foreach ($errors as $error): ?>
+                                            <li> - <?php echo $error; ?></li>
+                                        <?php endforeach; ?>
+                                    </ul>
+                                <?php endif; ?>
+
+                                <p>Fill out the form below to place your order. Our manager will contact you shortly.</p>
+
+                                <div class="login-form">
+                                    <form action="#" method="post">
+
+                                        <p>Your Name</p>
+                                        <input type="text" name="userName" placeholder="" value="<?php echo htmlspecialchars($userName); ?>"/>
+
+                                        <p>Phone Number</p>
+                                        <input type="text" name="userPhone" placeholder="" value="<?php echo htmlspecialchars($userPhone); ?>"/>
+
+                                        <p>Comment on the order</p>
+                                        <input type="text" name="userComment" placeholder="Message" value="<?php echo htmlspecialchars($userComment); ?>"/>
+
+                                        <br/>
+                                        <br/>
+                                        <input type="submit" name="submit" class="btn btn-default" value="Place Order" />
+                                    </form>
+                                </div>
+                            </div>
+
+                        <?php endif; ?>
+
+                    </div>
+
+                </div>
             </div>
-    </div>
-    </div>
+        </div>
     </section>
 
 
--- views/cart/index.php
+++ views/cart/index.php
@@ -1,79 +1,88 @@
-<?php include ROOT . '/views/layouts/header.php'; ?>
+<?php
+/**
+ * @var array $categories       Array of categories for the left sidebar navigation.
+ * @var array $productsInCart   Associative array mapping product IDs to the quantity selected by the user.
+ * @var array $products         Array of detailed product information (fetched from the database based on IDs in $productsInCart). Each product item has 'id', 'code', 'tittle', and 'price'.
+ * @var float $totalPrice       The calculated total cost of all items in the cart.
+ */
+include ROOT . '/views/layouts/header.php'; ?>
 
-<section>
-    <div class="container">
-        <div class="row">
-            <div class="col-sm-3">
-                <div class="left-sidebar">
-                    <h2>Catalog</h2>
-                    <div class="panel-group category-products">
-                        <?php foreach ($categories as $categoryItem): ?>
-                            <div class="panel panel-default">
-                                <div class="panel-heading">
-                                    <h4 class="panel-title">
-                                        <a href="/alias/c<?php echo $categoryItem['id']; ?>">
-                                            <?php echo $categoryItem['name']; ?>
-                                        </a>
-                                    </h4>
+    <section>
+        <div class="container">
+            <div class="row">
+                <div class="col-sm-3">
+                    <div class="left-sidebar">
+                        <h2>Catalog</h2>
+                        <div class="panel-group category-products">
+                            <?php foreach ($categories as $categoryItem): ?>
+                                <div class="panel panel-default">
+                                    <div class="panel-heading">
+                                        <h4 class="panel-title">
+                                            <a href="/alias/c<?php echo $categoryItem['id']; ?>">
+                                                <?php echo $categoryItem['name']; ?>
+                                            </a>
+                                        </h4>
+                                    </div>
                                 </div>
-                            </div>
-                        <?php endforeach; ?>
+                            <?php endforeach; ?>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-sm-9 padding-right">
+                    <div class="features_items">
+                        <h2 class="title text-center">Cart</h2>
+
+                        <?php if ($productsInCart): ?>
+                            <p>You have selected these products:</p>
+                            <table class="table-bordered table-striped table">
+                                <thead>
+                                <tr>
+                                    <th>Product Code</th>
+                                    <th>Name</th>
+                                    <th>Cost, UAH</th>
+                                    <th>Quantity, pcs</th>
+                                    <th>Delete</th>
+                                </tr>
+                                </thead>
+                                <tbody>
+                                <?php foreach ($products as $product): ?>
+                                    <tr>
+                                        <td><?php echo htmlspecialchars($product['code']);?></td>
+                                        <td>
+                                            <a href="/alias/p<?php echo $product['id'];?>">
+                                                <?php echo htmlspecialchars($product['tittle']);?>
+                                            </a>
+                                        </td>
+                                        <td><?php echo number_format($product['price'], 2);?></td>
+                                        <td><?php echo $productsInCart[$product['id']];?></td>
+                                        <td>
+                                            <a class="btn btn-default checkout" href="/cart/delete/<?php echo $product['id'];?>" title="Remove Item">
+                                                <i class="fa fa-times"></i>
+                                            </a>
+                                        </td>
+                                    </tr>
+                                <?php endforeach; ?>
+                                </tbody>
+                                <tfoot>
+                                <tr>
+                                    <td colspan="4" class="text-right"><strong>Total Cost, UAH:</strong></td>
+                                    <td><strong><?php echo number_format($totalPrice, 2);?></strong></td>
+                                </tr>
+                                </tfoot>
+                            </table>
+
+                            <a class="btn btn-default checkout" href="/cart/checkout"><i class="fa fa-shopping-cart"></i> Place Order</a>
+                        <?php else: ?>
+                            <p>Cart is empty.</p>
+
+                            <a class="btn btn-default checkout" href="/"><i class="fa fa-shopping-cart"></i> Return to Shopping</a>
+                        <?php endif; ?>
+
                     </div>
                 </div>
             </div>
-
-            <div class="col-sm-9 padding-right">
-                <div class="features_items">
-                    <h2 class="title text-center">Cart</h2>
-                    
-                    <?php if ($productsInCart): ?>
-                        <p>You have selected these products:</p>
-                        <table class="table-bordered table-striped table">
-                            <tr>
-                                <th>Product Code</th>
-                                <th>Name</th>
-                                <th>Cost, UAH</th>
-                                <th>Quantity, pcs</th>
-                                <th>Delete</th>
-                            </tr>
-                            <?php foreach ($products as $product): ?>
-                                <tr>
-                                    <td><?php echo $product['code'];?></td>
-                                    <td>
-                                        <a href="/alias/p<?php echo $product['id'];?>">
-                                            <?php echo $product['tittle'];?>
-                                        </a>
-                                    </td>
-                                    <td><?php echo $product['price'];?></td>
-                                    <td><?php echo $productsInCart[$product['id']];?></td>                        
-                                    <td>
-                                        <a class="btn btn-default checkout" href="/cart/delete/<?php echo $product['id'];?>">
-                                            <i class="fa fa-times"></i>
-                                        </a>
-                                    </td>                        
-                                </tr>
-                            <?php endforeach; ?>
-                                <tr>
-                                    <td colspan="4">Total cost, UAH:</td>
-                                    <td><?php echo $totalPrice;?></td>
-                                </tr>
-                            
-                        </table>
-                        
-                        <a class="btn btn-default checkout" href="/cart/checkout"><i class="fa fa-shopping-cart"></i> Place Order</a>
-                    <?php else: ?>
-                        <p>Cart пуста</p>
-                        
-                        <a class="btn btn-default checkout" href="/"><i class="fa fa-shopping-cart"></i> Return to Shopping</a>
-                    <?php endif; ?>
-
-                </div>
-
-                
-                
-            </div>
         </div>
-    </div>
-</section>
+    </section>
 
 <?php include ROOT . '/views/layouts/footer.php'; ?>
